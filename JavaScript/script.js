24028a0e8b48387b099dda50474c645e96a36b39
Manuel <santamari2097@gmail.com>
2022-05-12 15:33:41 -0500

    comienzo de proyecto

---
+++ JavaScript/script.js
@@ -0,0 +1,35 @@
+/*
+const ham =document.querySelector(".boton")
+const enlaces =document.querySelector(".responsive1")
+
+boton.addEventListener('click', () => {
+    enviar.classList.toggle('activado');
+});
+ham.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+});
+
+const boton = document.querySelector('boton');
+const responsive1 = document.querySelector('responsive1');
+
+
+
+boton.addEventListener("click", dia);
+
+function dia() {
+    responsive1.classList.toggle('activado');
+}
+const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.menu');
+    
+    
+
+
+ham.addEventListener('click', () => {
+    enlaces.classList.toggle("activado");
+});    
+*/
+
+
+
+
